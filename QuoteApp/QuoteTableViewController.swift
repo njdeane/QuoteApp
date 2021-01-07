b0488a845f0da2e2be160873f1f9fa568e4010c7
njdeane <njdeane7@gmail.com>
2021-01-07 17:17:48 +1000

    Populate reusable cells with free quotesArray.

--- QuoteApp/QuoteTableViewController.swift
+++ QuoteApp/QuoteTableViewController.swift
@@ -34,25 +34,18 @@ class QuoteTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        return quotesToShow.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+        cell.textLabel?.text = quotesToShow[indexPath.row]
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
